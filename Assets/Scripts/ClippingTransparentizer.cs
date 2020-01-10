34c28f1625fdbde4d8c0dbf08d8e6ce3b01a20fd
Tom Hyde <thyde@neworbit.co.uk>
2020-01-10 23:47:07 +0000

    Do some clipping handling

---
+++ Assets/Scripts/ClippingTransparentizer.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System;
+
+public class ClippingTransparentizer : MonoBehaviour
+{
+    private IEnumerable<Renderer> disabledRenderers = Array.Empty<Renderer>();
+
+    // Update is called once per frame
+    void Update()
+    {
+        var camera = this.GetComponentInChildren<Camera>();
+        if (camera == null)
+        {
+            return;
+        }
+
+        var vectorToParent = this.transform.position - camera.transform.position;
+        var rayHitsBetweenCameraAndParent = Physics.RaycastAll(camera.transform.position, vectorToParent, vectorToParent.magnitude);
+        var renderersToDisable = new List<Renderer>();
+        foreach (var hit in rayHitsBetweenCameraAndParent)
+        {
+            if (hit.collider.transform.IsChildOf(this.transform))
+            {
+                // Same game object - do not transparentize
+            }
+            else
+            {
+                var renderers = hit.collider.GetComponents<Renderer>();
+                renderersToDisable.AddRange(renderers);
+            }
+        }
+
+        foreach (var renderer in this.disabledRenderers.Except(renderersToDisable))
+        {
+            if (renderer != null)
+            {
+                renderer.enabled = true;
+            }
+        }
+
+        foreach(var renderer in renderersToDisable)
+        {
+            renderer.enabled = false;
+        }
+
+        this.disabledRenderers = renderersToDisable;
+    }
+}
