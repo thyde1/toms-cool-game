a652ab340b9f128a6714e665aadc02e34d86f9f0
Tom Hyde <thyde@neworbit.co.uk>
2020-01-11 14:25:35 +0000

    Added portal to end of level

---
+++ Assets/Scripts/Teleporter.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class Teleporter : MonoBehaviour
+{
+    public string Scene;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent<PlayerController>(out _))
+        {
+            SceneManager.LoadScene(this.Scene);
+            SceneManager.sceneLoaded += this.SceneLoaded;
+        }
+    }
+
+    private void SceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
+    {
+        SceneManager.SetActiveScene(scene);
+    }
+}
