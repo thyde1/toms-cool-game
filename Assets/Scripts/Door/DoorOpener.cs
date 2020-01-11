0685742e1b5cc425941fe668c82e70c7d109abe9
Tom Hyde <thyde@neworbit.co.uk>
2020-01-11 13:53:33 +0000

    Added opening doors and some sweet sfx

---
+++ Assets/Scripts/Door/DoorOpener.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOpener : MonoBehaviour
+{
+    public float MoveBy = 1;
+    public float OpenSpeed = 1;
+    private float initialY;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.initialY = this.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (this.transform.position.y < initialY + MoveBy)
+        {
+            this.transform.Translate(0, Time.deltaTime * this.OpenSpeed, 0);
+        }
+    }
+}
---
+++ Assets/Scripts/Door/DoorOpenerActivator.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorOpenerActivator : CollisionActivator<DoorOpener>
+{
+    public float MoveBy = 10;
+    public float OpenSpeed = 1;
+    public AudioClip OpenSound;
+
+    protected override void OnActivation(DoorOpener component)
+    {
+        component.MoveBy = this.MoveBy;
+        component.OpenSpeed = this.OpenSpeed;
+        if (OpenSound != null)
+        {
+            AudioSource.PlayClipAtPoint(this.OpenSound, component.transform.position);
+        }
+    }
+}
