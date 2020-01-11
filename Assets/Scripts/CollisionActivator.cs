f20ad572e5c479da4b483e7f2ba2dd38de9c194a
Tom Hyde <thyde@neworbit.co.uk>
2020-01-11 11:53:34 +0000

    Enemies come at you on trigger

---
+++ Assets/Scripts/CollisionActivator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public abstract class CollisionActivator<T> : MonoBehaviour where T : Component
+{
+    public GameObject[] ObjectsToActivate;
+    public GameObject ActivatedBy;
+    private bool activated = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (!this.activated && other.gameObject == this.ActivatedBy)
+        {
+            foreach (var objectToActivate in this.ObjectsToActivate) {
+                var component = objectToActivate.AddComponent<T>();
+                this.OnActivation(component);
+            }
+
+            this.activated = true;
+        }
+    }
+
+    protected abstract void OnActivation(T component);
+}
---
+++ Assets/Scripts/EnemyMoverActivator.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMoverActivator : CollisionActivator<EnemyMover>
+{
+    public float MoveSpeed;
+
+    protected override void OnActivation(EnemyMover component)
+    {
+        component.MoveSpeed = this.MoveSpeed;
+    }
+}
