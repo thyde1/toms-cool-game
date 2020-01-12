5f1aa124a6b670553e449410446fe2f48cb7f9ee
Tom Hyde <thyde@neworbit.co.uk>
2020-01-12 11:40:36 +0000

    Added some sweet textures

---
+++ Assets/Scripts/Rendering/HorizontalTiledTexture.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HorizontalTiledTexture : MonoBehaviour
+{
+    public float Scale = 1;
+
+    void Start()
+    {
+        var material = this.GetComponent<Renderer>().material;
+        var mesh = GetComponent<MeshFilter>().mesh;
+        material.mainTextureScale = new Vector2(mesh.bounds.size.x * transform.localScale.x * this.Scale, mesh.bounds.size.y * transform.localScale.y * this.Scale);
+    }
+}
---
+++ Assets/Scripts/Rendering/VerticalTiledTexture.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VerticalTiledTexture : MonoBehaviour
+{
+    public float Scale = 1;
+
+    void Start()
+    {
+        var material = this.GetComponent<Renderer>().material;
+        var mesh = GetComponent<MeshFilter>().mesh;
+        material.mainTextureScale = new Vector2(mesh.bounds.size.x * transform.localScale.x * this.Scale, mesh.bounds.size.z * transform.localScale.z * this.Scale);
+    }
+}
