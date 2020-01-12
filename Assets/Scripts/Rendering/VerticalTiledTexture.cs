36c9e05346f9d04c6038e568d5abc4c720a10351
Tom Hyde <thyde@neworbit.co.uk>
2020-01-12 15:01:05 +0000

    Tiled texture scripts work in editor

--- Assets/Scripts/Rendering/VerticalTiledTexture.cs
+++ Assets/Scripts/Rendering/VerticalTiledTexture.cs
@@ -1,14 +1,31 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+[ExecuteAlways]
 public class VerticalTiledTexture : MonoBehaviour
 {
     public float Scale = 1;
+    private Material material;
+    private Mesh mesh;
 
-    void Start()
+    private void Update()
     {
-        var material = this.GetComponent<Renderer>().material;
-        var mesh = GetComponent<MeshFilter>().mesh;
+        if (this.material == null || this.mesh == null)
+        {
+            this.GetMeshAndMaterial();
+        }
+
+        if (this.material == null || this.mesh == null)
+        {
+            return;
+        }
+
         material.mainTextureScale = new Vector2(mesh.bounds.size.x * transform.localScale.x * this.Scale, mesh.bounds.size.z * transform.localScale.z * this.Scale);
+    }
+
+    private void GetMeshAndMaterial()
+    {
+        this.material = this.GetComponent<Renderer>().sharedMaterial;
+        this.mesh = GetComponent<MeshFilter>().sharedMesh;
     }
 }
