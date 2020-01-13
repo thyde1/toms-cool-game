fe7b5c646d2a710b9196c684e8a37571cb7e07e7
Tom Hyde <thyde@neworbit.co.uk>
2020-01-13 11:33:07 +0000

    Enemies taunt you on death

---
+++ Assets/Scripts/Enemies/EnemyAnimator.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyAnimator : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        var animator = this.GetComponent<Animator>();
+        var target = this.GetComponentInParent<EnemyMover>()?.Target;
+        if (target == null)
+        {
+            // Taunt if you're dead
+            animator.ResetTrigger("Walk");
+            animator.SetTrigger("Taunt");
+            return;
+        }
+        if (target != null)
+        {
+            animator.ResetTrigger("Taunt");
+            animator.SetTrigger("Walk");
+        }
+    }
+}
--- Assets/Scripts/Enemies/EnemyMover.cs
+++ Assets/Scripts/Enemies/EnemyMover.cs
@@ -4,25 +4,25 @@ using System.Collections;
 public class EnemyMover : MonoBehaviour
 {
     public float MoveSpeed = 1;
-    private GameObject target;
+    public GameObject Target { get; set; }
     private CharacterController characterController;
 
     // Use this for initialization
     void Start()
     {
-        this.target = FindObjectOfType<PlayerController>().gameObject;
+        this.Target = FindObjectOfType<PlayerController>().gameObject;
         this.characterController = this.GetComponent<CharacterController>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (target == null)
+        if (Target == null)
         {
             return;
         }
 
-        var target2dPosition = new Vector3(this.target.transform.position.x, 0, this.target.transform.position.z);
+        var target2dPosition = new Vector3(this.Target.transform.position.x, 0, this.Target.transform.position.z);
         this.gameObject.transform.LookAt(target2dPosition);
         this.characterController.Move(this.transform.TransformVector(new Vector3(0, 0, Time.deltaTime * this.MoveSpeed)));
     }
